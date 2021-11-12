446ed5784ca83eaa1fe483adbb8400adf959bfac
DevExpressExampleBot <fakeEmail>
2021-11-12 13:53:13 +0300

    autogenerate VB

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,130 +1,134 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace Q134867
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
-			Me.nwindDataSet = New Q134867.nwindDataSet()
-			Me.productReportsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-			Me.productReportsTableAdapter = New Q134867.nwindDataSetTableAdapters.ProductReportsTableAdapter()
-			Me.fieldCategoryName = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldProductName = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldProductSales = New DevExpress.XtraPivotGrid.PivotGridField()
-			Me.fieldShippedDate = New DevExpress.XtraPivotGrid.PivotGridField()
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.productReportsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' pivotGridControl1
-			' 
-			Me.pivotGridControl1.Anchor = (CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
-			Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.Default
-			Me.pivotGridControl1.DataSource = Me.productReportsBindingSource
-			Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() { Me.fieldCategoryName, Me.fieldProductName, Me.fieldProductSales, Me.fieldShippedDate})
-			Me.pivotGridControl1.Location = New System.Drawing.Point(13, 13)
-			Me.pivotGridControl1.Name = "pivotGridControl1"
-			Me.pivotGridControl1.Size = New System.Drawing.Size(695, 383)
-			Me.pivotGridControl1.TabIndex = 0
-'			Me.pivotGridControl1.ShowMenu += New DevExpress.XtraPivotGrid.PivotGridMenuEventHandler(Me.pivotGridControl1_ShowMenu);
-			' 
-			' nwindDataSet
-			' 
-			Me.nwindDataSet.DataSetName = "nwindDataSet"
-			Me.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-			' 
-			' productReportsBindingSource
-			' 
-			Me.productReportsBindingSource.DataMember = "ProductReports"
-			Me.productReportsBindingSource.DataSource = Me.nwindDataSet
-			' 
-			' productReportsTableAdapter
-			' 
-			Me.productReportsTableAdapter.ClearBeforeFill = True
-			' 
-			' fieldCategoryName
-			' 
-			Me.fieldCategoryName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
-			Me.fieldCategoryName.AreaIndex = 0
-			Me.fieldCategoryName.Caption = "CategoryName"
-			Me.fieldCategoryName.FieldName = "CategoryName"
-			Me.fieldCategoryName.Name = "fieldCategoryName"
-			' 
-			' fieldProductName
-			' 
-			Me.fieldProductName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
-			Me.fieldProductName.AreaIndex = 1
-			Me.fieldProductName.Caption = "ProductName"
-			Me.fieldProductName.FieldName = "ProductName"
-			Me.fieldProductName.Name = "fieldProductName"
-			' 
-			' fieldProductSales
-			' 
-			Me.fieldProductSales.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
-			Me.fieldProductSales.AreaIndex = 0
-			Me.fieldProductSales.Caption = "ProductSales"
-			Me.fieldProductSales.FieldName = "ProductSales"
-			Me.fieldProductSales.Name = "fieldProductSales"
-			' 
-			' fieldShippedDate
-			' 
-			Me.fieldShippedDate.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
-			Me.fieldShippedDate.AreaIndex = 0
-			Me.fieldShippedDate.Caption = "Year"
-			Me.fieldShippedDate.FieldName = "ShippedDate"
-			Me.fieldShippedDate.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear
-			Me.fieldShippedDate.Name = "fieldShippedDate"
-			Me.fieldShippedDate.UnboundFieldName = "fieldShippedDate"
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(720, 408)
-			Me.Controls.Add(Me.pivotGridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.nwindDataSet, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.productReportsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
+            Me.nwindDataSet = New Q134867.nwindDataSet()
+            Me.productReportsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.productReportsTableAdapter = New Q134867.nwindDataSetTableAdapters.ProductReportsTableAdapter()
+            Me.fieldCategoryName = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldProductName = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldProductSales = New DevExpress.XtraPivotGrid.PivotGridField()
+            Me.fieldShippedDate = New DevExpress.XtraPivotGrid.PivotGridField()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.nwindDataSet), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.productReportsBindingSource), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' pivotGridControl1
+            ' 
+            Me.pivotGridControl1.Anchor = CType(((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+            Me.pivotGridControl1.Cursor = System.Windows.Forms.Cursors.[Default]
+            Me.pivotGridControl1.DataSource = Me.productReportsBindingSource
+            Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() {Me.fieldCategoryName, Me.fieldProductName, Me.fieldProductSales, Me.fieldShippedDate})
+            Me.pivotGridControl1.Location = New System.Drawing.Point(13, 13)
+            Me.pivotGridControl1.Name = "pivotGridControl1"
+            Me.pivotGridControl1.Size = New System.Drawing.Size(695, 383)
+            Me.pivotGridControl1.TabIndex = 0
+            Me.pivotGridControl1.PopupMenuShowing += New DevExpress.XtraPivotGrid.PopupMenuShowingEventHandler(AddressOf pivotGridControl1_PopupMenuShowing)
+            ' 
+            ' nwindDataSet
+            ' 
+            Me.nwindDataSet.DataSetName = "nwindDataSet"
+            Me.nwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            ' 
+            ' productReportsBindingSource
+            ' 
+            Me.productReportsBindingSource.DataMember = "ProductReports"
+            Me.productReportsBindingSource.DataSource = Me.nwindDataSet
+            ' 
+            ' productReportsTableAdapter
+            ' 
+            Me.productReportsTableAdapter.ClearBeforeFill = True
+            ' 
+            ' fieldCategoryName
+            ' 
+            Me.fieldCategoryName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
+            Me.fieldCategoryName.AreaIndex = 0
+            Me.fieldCategoryName.Caption = "CategoryName"
+            Me.fieldCategoryName.FieldName = "CategoryName"
+            Me.fieldCategoryName.Name = "fieldCategoryName"
+            ' 
+            ' fieldProductName
+            ' 
+            Me.fieldProductName.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
+            Me.fieldProductName.AreaIndex = 1
+            Me.fieldProductName.Caption = "ProductName"
+            Me.fieldProductName.FieldName = "ProductName"
+            Me.fieldProductName.Name = "fieldProductName"
+            ' 
+            ' fieldProductSales
+            ' 
+            Me.fieldProductSales.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
+            Me.fieldProductSales.AreaIndex = 0
+            Me.fieldProductSales.Caption = "ProductSales"
+            Me.fieldProductSales.FieldName = "ProductSales"
+            Me.fieldProductSales.Name = "fieldProductSales"
+            ' 
+            ' fieldShippedDate
+            ' 
+            Me.fieldShippedDate.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
+            Me.fieldShippedDate.AreaIndex = 0
+            Me.fieldShippedDate.Caption = "Year"
+            Me.fieldShippedDate.FieldName = "ShippedDate"
+            Me.fieldShippedDate.GroupInterval = DevExpress.XtraPivotGrid.PivotGroupInterval.DateYear
+            Me.fieldShippedDate.Name = "fieldShippedDate"
+            Me.fieldShippedDate.UnboundFieldName = "fieldShippedDate"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(720, 408)
+            Me.Controls.Add(Me.pivotGridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.nwindDataSet), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.productReportsBindingSource), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private WithEvents pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-		Private nwindDataSet As nwindDataSet
-		Private productReportsBindingSource As System.Windows.Forms.BindingSource
-		Private productReportsTableAdapter As Q134867.nwindDataSetTableAdapters.ProductReportsTableAdapter
-		Private fieldCategoryName As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldProductName As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldProductSales As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldShippedDate As DevExpress.XtraPivotGrid.PivotGridField
-	End Class
+#End Region
+        Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
+
+        Private nwindDataSet As Q134867.nwindDataSet
+
+        Private productReportsBindingSource As System.Windows.Forms.BindingSource
+
+        Private productReportsTableAdapter As Q134867.nwindDataSetTableAdapters.ProductReportsTableAdapter
+
+        Private fieldCategoryName As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldProductName As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldProductSales As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldShippedDate As DevExpress.XtraPivotGrid.PivotGridField
+    End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,69 +1,58 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.Utils.Menu
 Imports DevExpress.XtraPivotGrid
 
 Namespace Q134867
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			' TODO: This line of code loads data into the 'nwindDataSet.ProductReports' table. You can move, or remove it, as needed.
-			Me.productReportsTableAdapter.Fill(Me.nwindDataSet.ProductReports)
+    Public Partial Class Form1
+        Inherits Form
 
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-        Private Sub pivotGridControl1_ShowMenu(ByVal sender As Object, ByVal e As DevExpress.XtraPivotGrid.PopupMenuShowingEventArgs) Handles pivotGridControl1.PopupMenuShowing
-            If e.MenuType = DevExpress.XtraPivotGrid.PivotGridMenuType.Header Then
-                HandleHeaderShowMenu(e)
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ' TODO: This line of code loads data into the 'nwindDataSet.ProductReports' table. You can move, or remove it, as needed.
+            productReportsTableAdapter.Fill(nwindDataSet.ProductReports)
+        End Sub
+
+        Private Sub pivotGridControl1_PopupMenuShowing(ByVal sender As Object, ByVal e As DevExpress.XtraPivotGrid.PopupMenuShowingEventArgs)
+            If e.MenuType Is DevExpress.XtraPivotGrid.PivotGridMenuType.Header Then
+                Me.HandleHeaderShowMenu(e)
             End If
         End Sub
 
         Private Sub HandleHeaderShowMenu(ByVal e As DevExpress.XtraPivotGrid.PopupMenuShowingEventArgs)
-            If (Not IsLastAreaField(pivotGridControl1, e.Field)) Then
-                CreateToggleTotalsItem(e)
-            End If
+            If Not IsLastAreaField(pivotGridControl1, e.Field) Then Me.CreateToggleTotalsItem(e)
         End Sub
 
-		Private Function IsLastAreaField(ByVal pivotGridControl As PivotGridControl, ByVal field As PivotGridField) As Boolean
-			For i As Integer = 0 To pivotGridControl.Fields.Count - 1
-				If pivotGridControl.Fields(i).Visible AndAlso pivotGridControl.Fields(i).Area = field.Area AndAlso pivotGridControl.Fields(i).AreaIndex > field.AreaIndex Then
-					Return False
-				End If
-			Next i
-			Return True
-		End Function
+        Private Function IsLastAreaField(ByVal pivotGridControl As PivotGridControl, ByVal field As PivotGridField) As Boolean
+            For i As Integer = 0 To pivotGridControl.Fields.Count - 1
+                If pivotGridControl.Fields(i).Visible AndAlso pivotGridControl.Fields(i).Area Is field.Area AndAlso pivotGridControl.Fields(i).AreaIndex > field.AreaIndex Then Return False
+            Next
+
+            Return True
+        End Function
 
         Private Sub CreateToggleTotalsItem(ByVal e As DevExpress.XtraPivotGrid.PopupMenuShowingEventArgs)
-            Dim isTotalsVisible As Boolean = e.Field.TotalsVisibility <> PivotTotalsVisibility.None
-            Dim toggleTotalItem As DXMenuItem
-            If isTotalsVisible Then
-                toggleTotalItem = New DXMenuItem("Hide Totals", AddressOf ToggleTotalsMenuItemClick)
-            Else
-                toggleTotalItem = New DXMenuItem("Show Totals", AddressOf ToggleTotalsMenuItemClick)
-            End If
+            Dim isTotalsVisible As Boolean = e.Field.TotalsVisibility IsNot PivotTotalsVisibility.None
+            Dim toggleTotalItem As DXMenuItem = New DXMenuItem(If(isTotalsVisible, "Hide Totals", "Show Totals"), AddressOf ToggleTotalsMenuItemClick)
             toggleTotalItem.BeginGroup = True
             toggleTotalItem.Tag = e.Field
             e.Menu.Items.Add(toggleTotalItem)
         End Sub
 
-		Private Sub ToggleTotalsMenuItemClick(ByVal sender As Object, ByVal e As EventArgs)
-			Dim item As DXMenuItem = CType(sender, DXMenuItem)
-			Dim field As PivotGridField = CType(item.Tag, PivotGridField)
-			If field.TotalsVisibility = PivotTotalsVisibility.None Then
-				field.TotalsVisibility = PivotTotalsVisibility.AutomaticTotals
-			Else
-				field.TotalsVisibility = PivotTotalsVisibility.None
-			End If
-		End Sub
-	End Class
+        Private Sub ToggleTotalsMenuItemClick(ByVal sender As Object, ByVal e As EventArgs)
+            Dim item As DXMenuItem = CType(sender, DXMenuItem)
+            Dim field As PivotGridField = CType(item.Tag, PivotGridField)
+            If field.TotalsVisibility Is PivotTotalsVisibility.None Then
+                field.TotalsVisibility = PivotTotalsVisibility.AutomaticTotals
+            Else
+                field.TotalsVisibility = PivotTotalsVisibility.None
+            End If
+        End Sub
+    End Class
 End Namespace
